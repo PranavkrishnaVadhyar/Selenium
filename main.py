a248d7b3c9bc7b39264dc0451b45f8f5215e7da0
B Pranavkrishna Vadhyar <bpranavkrishnavadhyar@gmail.com.com>
2024-07-03 21:50:34 +0530

    test commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+#sample commit
 query = 'laptop'
 driver.get(f"https://www.amazon.com/s?k={query}&crid=3FCLG8QYEJEX9&sprefix=laptop%2Caps%2C403&ref=nb_sb_noss_1")
 element = driver.find_element(By.CLASS_NAME,"puisg-col-inner")
