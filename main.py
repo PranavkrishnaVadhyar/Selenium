abdbc23caf8a22213104ee74129ef3eb867b2130
B Pranavkrishna Vadhyar <bpranavkrishnavadhyar@gmail.com.com>
2024-07-05 12:35:38 +0530

    Sample program

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-#sample commit
-query = 'laptop'
-driver.get(f"https://www.amazon.com/s?k={query}&crid=3FCLG8QYEJEX9&sprefix=laptop%2Caps%2C403&ref=nb_sb_noss_1")
-element = driver.find_element(By.CLASS_NAME,"puisg-col-inner")
-print(element.text)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pyconioshiosajciahdc")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+print(a.text)
+driver.close()
