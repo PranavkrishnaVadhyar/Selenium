d519a455144290231ba4706aecff04f7be531404
B Pranavkrishna Vadhyar <bpranavkrishnavadhyar@gmail.com>
2024-07-05 12:40:35 +0530

    user verification

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
+
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element(By.NAME, "q")
